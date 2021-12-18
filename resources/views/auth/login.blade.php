837f95053dc801471ccafd7b0df6169e965506fe
Josh Jackson <joshpjackson12@gmail.com>
2021-12-18 12:23:59 +0000

    updated border

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,17 @@
 <div class="bg-purple flex w-100 h-full items-center justify-center">
     <div class="bg-white rounded-xl shadow h-fit p-10">
         <img src="/images/logo_purple.png" alt="logo" class="w-1/2 mx-auto">
-        <h1 class="font-header text-gray-700 text-3xl mt-8 text-center">Please log in</h1>
-        <form class="grid grid-rows-2 grid-cols-min-auto-1">
-            <label for="email" class="mb-2 text-lg text-sans w-min">Email</label>
+        <h1 class="font-header text-gray-700 text-3xl my-8 text-center">Please log in</h1>
+        <form class="grid grid-rows-2 grid-cols-min-auto-1 gap-x-2" id="login">
+            <label for="email" class="mb-2 text-lg text-sans w-min mt-2">Email</label>
             <input type="email" class="mb-4 py-2 px-5 d-block w-full rounded-full border border-2 focus:outline-none focus:border-purple hover:border-purple text-gray-400" name="email" id="email" placeholder="me@example.com">
-            <label for="password" class="mb-2 text-lg text-sans w-min">Password</label>
+            <label for="password" class="mb-2 text-lg text-sans w-min mt-2">Password</label>
             <input type="password" class="mb-4 py-2 px-5 d-block w-full rounded-full border border-2 focus:outline-none focus:border-purple hover:border-purple text-gray-400" name="password" id="password">
         </form>
+        <div class="flex justify-between">
+            <a href="#" class="border-blue-200 border-2 text-blue-200 px-4 py-2 rounded-full">Create Account</a>
+            <button type="submit" form="login" class="text-white bg-purple rounded-full px-4 py-2 ring-4 ring-transparent hover:ring-fuchsia-300">Sign In</button>
+        </div>
     </div>
 </div>
 
