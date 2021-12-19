5650b16cd13c0688b8c0750cebb985ff48b959c1
joshjackson <joshpjackson12@gmail.com>
2021-12-19 21:06:32 +0000

    added some form attributes for submission and added the csrf token

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,8 @@
     <div class="bg-white rounded-xl shadow h-fit p-10">
         <img src="/images/logo_purple.png" alt="logo" class="w-1/2 mx-auto">
         <h1 class="font-header text-gray-700 text-3xl my-8 text-center">Please log in</h1>
-        <form class="grid grid-rows-2 grid-cols-min-auto-1 gap-x-2" id="login">
+        <form class="grid grid-rows-2 grid-cols-min-auto-1 gap-x-2" id="login" action="/login" method="post">
+            @csrf
             <label for="email" class="mb-2 text-lg text-sans w-min mt-2 font-thin text-right">Email</label>
             <input type="email" class="mb-4 py-2 px-5 d-block w-full rounded-full border border-2 focus:outline-none focus:border-purple hover:border-purple text-gray-400" name="email" id="email" placeholder="me@example.com">
             <label for="password" class="mb-2 text-lg text-sans w-min mt-2 font-thin">Password</label>
