687e0e118fed3b2abc81a355eefae93737a11417
joshjackson <joshpjackson12@gmail.com>
2022-01-07 20:58:59 +0000

    finished tabs blade component

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -1,3 +1,3 @@
-<div x-show="tab === '{{ $slug }}'" class="bg-white">
+<div x-show="tab === '{{ $slug }}'" class="bg-white border rounded-b-xl rounded-r-xl p-5">
     {{ $slot }}
 </div>
--- resources/views/components/tabs.blade.php
+++ resources/views/components/tabs.blade.php
@@ -1,7 +1,7 @@
 <div x-data="{ tab: window.location.hash ? window.location.hash.substring(1) : '{{ array_key_first($tabs) }}'}" id="tab_wrapper">
-    <nav>
+    <nav class="relative mb-2 top-0.5">
         @foreach ($tabs as $slug => $title)
-            <a class="bg-white" :class="{ 'text-purple': tab === '{{ $slug }}' }" @click.prevent="tab = '{{ $slug }}'; window.location.hash = '{{ $slug }}'" href="#">{{ $title }}</a>
+            <a class="bg-white px-4 py-2 border border-b-white rounded-t-xl" :class="{ 'text-purple z-10 border-b-2': tab === '{{ $slug }}', 'top-0.5': tab !== '{{ $slug }}' }" @click.prevent="tab = '{{ $slug }}'; window.location.hash = '{{ $slug }}'" href="#">{{ $title }}</a>
         @endforeach
     </nav>
     {{ $slot }}
