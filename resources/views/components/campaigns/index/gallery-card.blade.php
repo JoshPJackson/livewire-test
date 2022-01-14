4509404a119c12daff448f3582c074cf2d696818
joshjackson <joshpjackson12@gmail.com>
2022-01-14 21:58:01 +0000

    did some reformatting on the gallery cards

--- resources/views/components/campaigns/index/gallery-card.blade.php
+++ resources/views/components/campaigns/index/gallery-card.blade.php
@@ -1,12 +1,12 @@
-<div class="bg-white rounded-xl border shadow w-100 grid grid-cols-auto-min-1 hover:border-purple hover:bg-purple-100 hover:cursor-pointer" @click="window.location.href='{{ route('campaigns.view', ['campaign' => $campaignId]) }}'">
+<div class="bg-white rounded-xl border shadow w-100 grid grid-cols-auto-min-1 hover:border-blue-300 hover:bg-blue-100 hover:cursor-pointer text-gray-500" @click="window.location.href='{{ route('campaigns.view', ['campaign' => $campaignId]) }}'">
     <div class="px-5 py-5">
-        <h1 class="font-header text-3xl text-purple whitespace-nowrap mb-4" style="word-break: keep-all">{{ $name }}</h1>
+        <x-text.h2>{{ $name }}</x-text.h2>
         <div>
             <div>{{ mt_rand(0, 10000) }} interactions</div>
             <div>{{ mt_rand(0, 3000) }} unique interactions</div>
         </div>
     </div>
-    <div class="grid grid-rows-3 border-gray-100 border-l text-xl">
+    <div class="grid grid-rows-3 border-gray-100 border-l">
         <a href="{{ route("campaigns.view", ['campaign' => $campaignId]) }}" class="flex items-center hover:cursor-pointer px-3 border-b border-gray-100 hover:text-blue-300">
             <i class="lni lni-enter"></i>
         </a>
