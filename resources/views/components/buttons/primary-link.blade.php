82056b60045cb26012c2a5ddc1fe0fef7707b794
joshjackson <joshpjackson12@gmail.com>
2022-01-14 21:57:31 +0000

    updated primary-link.blade.php to make sure contents are always vertically centered

--- resources/views/components/buttons/primary-link.blade.php
+++ resources/views/components/buttons/primary-link.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => "text-white bg-purple rounded-full px-4 py-2 ring-4 ring-transparent hover:ring-fuchsia-300"]) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => "text-white bg-purple rounded-full px-4 py-2 ring-4 ring-transparent hover:ring-fuchsia-300 flex items-center"]) }}>{{ $slot }}</a>
