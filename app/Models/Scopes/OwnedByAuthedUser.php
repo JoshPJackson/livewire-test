da9d6bfc3c4f185c0eaa7346af7502ad9b504bc2
joshjackson <joshpjackson12@gmail.com>
2022-01-14 21:54:32 +0000

    updated scope so foreign key is passed through constructor instead

--- app/Models/Scopes/OwnedByAuthedUser.php
+++ app/Models/Scopes/OwnedByAuthedUser.php
@@ -6,10 +6,18 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
+/**
+ * Add where for 'user_id' = auth()->user()->id
+ */
 class OwnedByAuthedUser implements Scope
 {
-    public function apply(Builder $builder, Model $model, string $foreignKey = 'user_id')
+    public function __construct(public string $foreignKey = 'user_id')
     {
-        $builder->where($foreignKey, auth()->user()->id);
+
+    }
+
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->where($this->foreignKey, auth()->user()->id);
     }
 }
