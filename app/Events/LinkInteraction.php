457bf3d1610f946b5c56d884286d28a46d14aa44
joshjackson <joshpjackson12@gmail.com>
2022-01-15 20:12:50 +0000

    added link interaction event

---
+++ app/Events/LinkInteraction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Link;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Http\Request;
+use Illuminate\Queue\SerializesModels;
+
+class LinkInteraction
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(public Request $request, public Link $link)
+    {
+        //
+    }
+}
--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\LinkInteraction;
 use App\Models\Link;
 use Illuminate\Http\Request;
 
@@ -12,13 +13,18 @@ class LinkController extends Controller
         // grab path
         $path = $request->path();
 
-        // check if kin exists with path
+        // check if link exists with path
         $resolvedLink = Link::where('path', $path)->first();
 
+        // if no link exists, just 404 the request
         if (!$resolvedLink) {
             abort(404);
         }
 
+        // link does exist, so log the interaction
+        LinkInteraction::dispatch($request, $resolvedLink);
+
+        // then send the user to their destination
         return redirect($resolvedLink->destination);
     }
 }
