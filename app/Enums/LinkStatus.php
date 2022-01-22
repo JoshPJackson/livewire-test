397f834541793eb7c1ac00dd64d49af997c644d4
joshjackson <joshpjackson12@gmail.com>
2022-01-22 14:26:30 +0000

    added LinkStatus enum to represent the states of links

---
+++ app/Enums/LinkStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum LinkStatus: string
+{
+    case ACTIVE = 'Active'; // Link is currently attached to an active campaign and can be hit
+    case INACTIVE = 'Inactive'; // Link is currently not being used for any live campaigns, so cannot be hit
+    case DISABLED = 'Disabled'; // Link is currently attached to a paused live campaign
+
+    public static function toArray()
+    {
+        return array_combine(array_column(self::cases(), 'name'), array_column(self::cases(), 'value'));
+    }
+}
