c9452880d98490ef844ce5b4bf70232010168101
joshjackson <joshpjackson12@gmail.com>
2022-01-22 21:47:04 +0000

    changed text colour for select

--- resources/views/components/select-input.blade.php
+++ resources/views/components/select-input.blade.php
@@ -1,4 +1,4 @@
-<select {{ $attributes->merge(['class' => 'bg-white rounded-full border hover:border-purple hover:cursor-pointer appearance-none px-4 focus:outline-none']) }}>
+<select {{ $attributes->merge(['class' => 'bg-white rounded-full border hover:border-purple hover:cursor-pointer appearance-none px-4 focus:outline-none text-gray-400']) }}>
     @foreach ($options as $value => $label)
         <option value="{{ $useLabelOnly ? $label : $value }}">{{ $label }}</option>
     @endforeach
